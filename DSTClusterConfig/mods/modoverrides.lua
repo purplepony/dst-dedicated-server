57b1307a45243fcebbdaf774a572a35e8ef3840a
Lava <nobody@example.com>
2018-04-02 22:43:36 -0400

    Documented each of the enabled mods for easy future reference.

--- DSTClusterConfig/mods/modoverrides.lua
+++ DSTClusterConfig/mods/modoverrides.lua
@@ -13,9 +13,13 @@ return {
   --   },
   --   enabled=true
   -- },
+  -- Riko (custom character from Made in Abyss anime) - https://steamcommunity.com/sharedfiles/filedetails/?id=1181077385
   ["workshop-1181077385"]={ configuration_options={  }, enabled=true },
+  -- Reg (custom character from Made in Abyss anime) - https://steamcommunity.com/sharedfiles/filedetails/?id=1217385169
   ["workshop-1217385169"]={ configuration_options={  }, enabled=true },
+  -- Nanachi (custom character from Made in Abyss anime) - https://steamcommunity.com/sharedfiles/filedetails/?id=1166220990
   ["workshop-1166220990"]={ configuration_options={ language=2 }, enabled=true },
+  -- Global positions mod - https://steamcommunity.com/sharedfiles/filedetails/?id=378160973
   ["workshop-378160973"]={
     configuration_options={
       ENABLEPINGS=true,
@@ -28,5 +32,6 @@ return {
     },
     enabled=true
   },
+  -- Finder mod (helps managing resources by highlighting chests with active item) - https://steamcommunity.com/sharedfiles/filedetails/?id=786654500
   ["workshop-786654500"]={ configuration_options={ active=true }, enabled=true }
 }
