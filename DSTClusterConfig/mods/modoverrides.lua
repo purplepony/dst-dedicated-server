f13067a476a9b0b81794621c59e864b462b6c734
Lava <lava@steamrep.com>
2018-10-23 23:08:19 -0400

    Re-added documentation to mod settings template.

--- DSTClusterConfig/mods/modoverrides.lua
+++ DSTClusterConfig/mods/modoverrides.lua
@@ -1,3 +1,9 @@
+-- Use this file to enable and configure your mods. The mod will only be available in the game
+-- if you set "enabled=true"!!!
+--
+-- Also, during the container startup this file will be copied to both Master/ and Caves/ folders. What's setup here
+-- will be available in both shards!
+
 return {
 -- Nanachi
   ["workshop-1166220990"]={ configuration_options={ attract=1, language=1 }, enabled=true },
