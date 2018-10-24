59a8a4a30c964f47ea771bf3686389a5f809cf37
Lava <lava@steamrep.com>
2018-10-24 00:13:33 -0400

    Added standalone scripts to launch each DST server for interactive command-line interface.

---
+++ caves.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+if [ -z $1 ]; then
+  cluster_name="TheAbyss"
+else
+  cluster_name="$1"
+fi
+
+cd ~/server_dst/bin
+./dontstarve_dedicated_server_nullrenderer -console -cluster $cluster_name -shard Caves
