3e610f1b7e27e06521f2f1ccec66a2e7fabb97e9
Lava <lava@steamrep.com>
2018-10-26 19:05:42 -0400

    Update game server before each time main world is launched.

--- master.sh
+++ master.sh
@@ -6,5 +6,10 @@ else
   cluster_name="$1"
 fi
 
+# Install DST server if not present, update if it's already installed.
+pushd ~ 1&>/dev/null
+./steamcmd.sh +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +login anonymous +force_install_dir /home/dst/server_dst +app_update 343050 validate +quit
+popd 1&>/dev/null
+
 cd ~/server_dst/bin
 ./dontstarve_dedicated_server_nullrenderer -console -cluster $cluster_name -shard Master
